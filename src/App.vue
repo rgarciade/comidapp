b94652ec5948b97f704389d8deee1a3d82811e92
raul garcia de la fuente <garcia.de.la.fuente.raul@gmail.com>
2023-01-08 13:06:42 +0100

    update package json

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,11 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <p>asfassfa</p>
-  <!-- <HelloWorld msg="Vite =+ Vue" />-->
-  <ExampleCompVue />
-  <div class="bg-blue-500">
-    <h1>holaafsa</h1>
-  </div>
-  <p>asfasfa</p>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from './components/HelloWorld.vue'
-  import ExampleCompVue from './components/ExampleComp.vue'
-  import '@/assets/tailwind.css'
+import "@/assets/tailwind.css";
 </script>
 
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
-</style>
+<style scoped></style>
