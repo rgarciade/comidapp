6aee7fccec541d7be80d1781eeb4b5e710d1b456
raul garcia de la fuente <garcia.de.la.fuente.raul@gmail.com>
2023-04-30 12:59:47 +0200

    fix pwa pluging error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -9,43 +8,7 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  plugins: [
-    vue(),
-    VitePWA({
-      mode: 'development',
-      base: '/',
-      srcDir: 'src',
-      filename: 'sw.ts',
-      includeAssets: ['/favicon.png'],
-      strategies: 'injectManifest',
-      manifest: {
-        name: 'Test Project',
-        short_name: 'Test',
-        theme_color: '#ffffff',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [vue()],
   build: {
     target: 'es2022',
   },
